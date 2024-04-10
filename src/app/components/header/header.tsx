a2ef58e8e6df5f9fef47d3a5cc08f8869b95eb7f
Fernando <fokamura10@gmail.com>
2024-04-10 20:11:55 -0300

    Alguns ajustes na parte de imagens para celular e pc

--- src/app/components/header/header.tsx
+++ src/app/components/header/header.tsx
@@ -3,12 +3,11 @@ import "./header.scss"
 export function Header (){
     return(
         <div className="header">
-      <span><Image src="/f.png" alt="perfil fer" width={120} height={100} priority></Image></span>
-            
            <div>
             <h1>Hi, I'm Fernando!</h1>
             <h2>Software developer</h2>
            </div>
+           <span><Image src="/fer.jpg" alt="perfil fer" width={120} height={100} priority></Image></span>
            </div>
      
     )
