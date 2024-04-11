e2e6f68602150659379010c7af04a90741cc1209
Fernando <fokamura10@gmail.com>
2024-04-10 22:29:46 -0300

    ajuste cor cinza e adequando os locais das cores

--- src/app/components/header/header.tsx
+++ src/app/components/header/header.tsx
@@ -4,10 +4,10 @@ export function Header (){
     return(
         <div className="header">
            <div>
-            <h1>Hi, I'm Fernando!</h1>
+            <h1>Hi, I'm Fernando! 👋</h1>
             <h2>Software developer</h2>
            </div>
-           <span><Image src="/fer.jpg" alt="perfil fer" width={120} height={100} priority></Image></span>
+           <span><Image src="/fer.jpg" alt="perfil fer" width={325} height={310} priority></Image></span>
            </div>
      
     )
